64b6326f2081bdeab89f38258c475f9a29bea189
Martin Blomberg <martin.blomberg@outlook.com>
2023-05-09 13:51:41 +0200

    Add save profile

---
+++ src/main/resources/static/js/profile.js
@@ -0,0 +1,27 @@
+const URL = "http://localhost:8080/users"
+
+const usernameinput = document.getElementById('userprofilename');
+const userimageinput = document.getElementById('userprofileimage');
+const saveprofile = document.getElementById('saveprofile');
+
+saveprofile.onclick = function(){
+    if( usernameinput.value !== "" && userimageinput !== ""){
+        const body = {
+            userName: usernameinput.value,
+            pictureLink: userimageinput.value
+        };
+        const url = URL;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'userID': userID
+            },
+            body: JSON.stringify(body)
+        })
+            .then(response => {
+                console.log("User profile updated");
+            })
+            .catch(err => console.error(err));
+    }
+}
